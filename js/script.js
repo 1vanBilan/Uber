92138e5a8a6ac7e633359091f377966cd8ccd47f
Ivan Bilan <94799578+1vanBilan@users.noreply.github.com>
2022-08-20 17:18:17 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+    window.addEventListener("DOMContentLoaded", ()=>{
+        const hamburger = document.querySelector('.hamburger');
+        const menu = document.querySelector(".menu");
+        hamburger.addEventListener("click", ()=>{
+        hamburger.classList.toggle("hamburger_active");
+        menu.classList.toggle('menu_active');
+    });
+    });
